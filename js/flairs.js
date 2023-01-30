dc2abefeccf56ba8175eac8ddbbde937760fb863
antonlabz <20198370+antonlabz@users.noreply.github.com>
2023-01-30 14:55:42 +1100

    Update for automatic updating loop using GET requests

--- js/flairs.js
+++ js/flairs.js
@@ -1,23 +1,25 @@
-loadPage = function() {
+var xhr = new XMLHttpRequest();
 
-	var flair_class = '';
+xhr.open("GET", "https://api.github.com/repos/dokkan-dfe/dokkan-dfe.github.io/contents/images/icons?ref=master", true);
 
-	//SPECIAL FLAIRS
-	loadSpecial = function() {
-		var enter = document.getElementById('special');
+xhr.onload = function() {
+	if (xhr.readyState === xhr.DONE) {
+		if (xhr.status === 200) {
+			// match strictly against the XML containing the file URL elements in server directory
+			var n = (xhr.response.match(/"name": "[0-9]{1,3}.png/g).length);
 
-		for (i=1; i<=107; i++) {
-			//creates HTML for special flairs
-			var flair_special = document.createElement('img');
-			flair_special.setAttribute('class', 'flair');
-			flair_special.setAttribute('id', i);
-			flair_special.setAttribute('src', 'images/icons/'+i+'.png');
+			var enter = document.getElementById('special');
 
-			enter.appendChild(flair_special);
+			// loop to generate flairs based on file count
+			for (var i = 1; i <= n; i++) {
+				var flair_special = document.createElement('img');
+				flair_special.setAttribute('class', 'flair');
+				flair_special.setAttribute('id', i);
+				flair_special.setAttribute('src', 'images/icons/'+i+'.png');
+				enter.appendChild(flair_special);
+			}
 		}
 	}
-	loadSpecial();
-}
+};
 
-//waits for DOM to load before executing function
-document.addEventListener('DOMContentLoaded', loadPage, false);
+xhr.send();
