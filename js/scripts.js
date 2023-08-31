7ffb1d8776b30c747eff3d99b45cde038e8c4016
antonlabz <20198370+antonlabz@users.noreply.github.com>
2023-08-31 20:57:59 +1000

    image generator to PNG

--- js/scripts.js
+++ js/scripts.js
@@ -176,7 +176,7 @@ function generateImage() {
 
   $(".modal-content").empty();
 
-  domtoimage.toSvg($('.icon-container')[0]).then(function (dataUrl) {
+  domtoimage.toPng($('.icon-container')[0]).then(function (dataUrl) {
           var img = new Image();
           img.src = dataUrl;
           $(".modal-content").append(img);
