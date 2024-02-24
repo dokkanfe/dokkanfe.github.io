a6e4ab460a74d991447e800436d83669c658e218
antonlabz <20198370+antonlabz@users.noreply.github.com>
2024-02-25 00:39:06 +1100

    added EZA icons

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,32 @@
 loadFlairs = function() {
   var enter = document.getElementById('special');
 
+  var eza = [
+    1,2,3,4,5,6,7,8,9,
+    10,11,12,13,14,15,16,17,18,19,
+    20,21,22,23,24,25,26,27,28,29,
+    30,31,32,33,34,35,36,37,38,39,
+    40,41,42,43,44,45,46,47,48,49,
+    50,51,52,53,54,55,56,57,58,59,
+    60,61,62,63,64,65,66,69,
+    71,74,
+  ]
+
   // length variable from fetch API below
   for (i=1; i<=total; i++) {
     //creates HTML for special flairs
-    var flair_special = document.createElement('img');
+    var flair_special = document.createElement('div');
     flair_special.setAttribute('class', 'flair');
     flair_special.setAttribute('id', i);
-    flair_special.setAttribute('src', 'images/icons/'+i+'.webp');
+    flair_special.style.backgroundImage='url(../images/icons/'+i+'.webp)';
 
     enter.appendChild(flair_special);
+  }
+
+  //assigns EZA class based on array
+  for (i in eza) {
+    current = eza[i];
+    $("#"+current).attr('class', 'flair eza');
   }
 }
 
@@ -260,7 +277,7 @@ $(document).ready(function() {
   });
 
   //main function for selecting icons
-  $("#special").on("click", "img", function(e) {
+  $("#special").on("click", "div", function(e) {
     var isChecked = document.getElementById('hide-lr').checked;
 
     const $obj = $(this);
