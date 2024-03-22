b05bc72a783094414f4bebb7cb6437794560622d
antonlabz <20198370+antonlabz@users.noreply.github.com>
2024-03-22 17:39:16 +1100

    Super EZA support, Saiyan Day Vegeta

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ loadFlairs = function() {
   var enter = document.getElementById('special');
 
   var eza = [
-    1,2,3,4,5,6,7,8,9,
+    1,2,3,5,6,7,8,9,
     10,11,12,13,14,15,16,17,18,19,
     20,21,22,23,24,25,26,27,28,29,
     30,31,32,33,34,35,36,37,38,39,
@@ -11,6 +11,10 @@ loadFlairs = function() {
     50,51,52,53,54,55,56,57,58,59,
     60,61,62,63,64,65,66,69,
     70,71,74,
+  ]
+
+  var eza2 = [
+    4
   ]
 
   // length variable from fetch API below
@@ -28,6 +32,12 @@ loadFlairs = function() {
   for (i in eza) {
     current = eza[i];
     $("#"+current).attr('class', 'flair eza');
+  }
+
+  //assigns SUPER EZA class based on array
+  for (i in eza2) {
+    current = eza2[i];
+    $("#"+current).attr('class', 'flair eza2');
   }
 }
 
